e71b5c5af08cfee0fe148ad1e0974ac5cf3b8999
Fady Morcos <fadymorcos22@gmail.com>
2025-04-30 20:11:57 -0400

    updated bio

--- src/sections/About.js
+++ src/sections/About.js
@@ -10,15 +10,16 @@ const About = () => {
         building scalable, intuitive software solutions. I earned my B.Eng. in
         Software Engineering with co-op from McMaster University, where I honed
         skills in languages like Python, C++, and JavaScript, alongside areas
-        like machine learning, computer architecture, and real-time systems.
+        like machine learning, control systems, computer architecture, data
+        sturctures and algorithms, advanced databases, and real-time systems.
       </p>
       <p>
         Currently, I freelance as a Software Engineer, developing full-stack
         systems for clients across industries. I work closely with clients to
         deliver tailored solutions, using technologies such as React, Next.js,
         Tailwind, Node.js, AWS Lambda, DynamoDB, MySQL, Google Cloud Platform,
-        and C#/.NET 8. Recent projects include pharmacy web systems with
-        integrated admin dashboards, streamlining operations like prescription
+        and Vercel. Recent projects include pharmacy web systems with integrated
+        administrative dashboards, streamlining operations like prescription
         management and appointment scheduling.
       </p>
       <p>
@@ -34,9 +35,10 @@ const About = () => {
       </p>
       <p>
         I also carry an entrepreneurial spirit, demonstrated by launching
-        Desktop Doctor — a service-booking platform for computer repairs. I
-        thrive in collaborative environments and am always excited to take on
-        meaningful projects that create real impact.
+        Desktop Doctor — a computer and phone repair business for which I
+        operated as a side hustle for some time. I also thrive in collaborative
+        environments and am always excited to take on meaningful projects that
+        create real impact.
       </p>
     </Section>
   );
